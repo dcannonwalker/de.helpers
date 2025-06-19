8a4986d227d4b886fd04ab57e936106d25f09799
dcannonwalker <42654459+dcannonwalker@users.noreply.github.com>
2025-06-19 15:23:01 -0500

    doc

--- R/estimate_parameters.R
+++ R/estimate_parameters.R
@@ -1,9 +1,13 @@
 #' Estimate parameters from a real rna-seq data set
 #'
-#' @param counts
-#' @param group
+#' @param counts a matrix of counts
+#' @param group a vector of treatment group labels corresponding to
+#' columns of `counts`
+#' @param min_count the minimum count to keep a tag in the dataset
+#' @param filter_fn function to apply rowwise; result is compared to `min_count`
+#' @export
 estimate_parameters <- function(counts, group, min_count = 10,
-                                filter_fn = sum, ...) {
+                                filter_fn = sum) {
     idx <- which(apply(counts, 1, filter_fn) < min_count)
     counts <- counts[idx, ]
     y <- DGEList(counts = counts, group = group)
