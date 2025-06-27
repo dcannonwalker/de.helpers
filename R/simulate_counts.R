2830158461e3cb61b12e58707150005105dc2b64
dcannonwalker <42654459+dcannonwalker@users.noreply.github.com>
2025-06-27 11:31:40 -0500

    fill in some effects simulation stuff

--- R/simulate_counts.R
+++ R/simulate_counts.R
@@ -21,30 +21,31 @@ sample_neighborhood <- function(x0, x, y, interval = 3) {
 
 #' Simulate treatment effects
 #' @inheritParams simulate_counts
-#' @param type One of `c("emp", "dst")`, for sampling from estimated
-#' coefficients or sampling from an exponential; if `type` is `"emp"`,
+#' @param method One of `c("emp", "dst")`, for sampling from estimated
+#' coefficients or sampling from an exponential; if `method` is `"emp"`,
 #' `mean_pars` must not be `NULL`
 #' @param de_prob Either a single probability representing the chance
 #' that any given tag exhibits differential expression,
 #' or a named vector of two probabilities, representing the chance
 #' that any given tag exhibits differential expression in the `up` or `down`
 #' direction; if a single probability, half of differentially expressed tags
-#' will be `up` regulated
-#' @param theta Exponential parameter to be used if `type` is `"dst"`
+#' will be `up` regulated; used only if `method` is `"dst"`
+#' @param theta Exponential parameter to be used if `method` is `"dst"`
 #' @param min_log_fc Minimum value added to draws from `"dst"`
 #' @param ... Additional arguments passed to sub-functions
-simulate_effects <- function(n_tags, mean_pars = NULL, type = c("dst", "emp"),
-                             de_prob = c(up = 0.05, down = 0.05),
+simulate_effects <- function(n_tags, mean_pars = NULL, method = c("dst", "emp"),
+                             de_prob = NULL,
                              theta = NULL, min_log_fc = log(1.5), ...) {
-    type <- match.arg(type)
-    if (type == "emp") {
+    method <- match.arg(method)
+    if (method == "emp") {
         if (is.null(mean_pars))
-            stop("If type is 'emp', mean_pars must be provided")
+            stop("If method is 'emp', mean_pars must be provided")
         effects <- simulate_effects.emp(n_tags = n_tags, mean_pars = mean_pars,
                                         de_prob = de_prob, ...)
     }
-    if (type == "dst") {
+    if (method == "dst") {
         theta <- theta %||% 1
+        de_prob <- de_prob %||% c(up = 0.05, down = 0.05)
         effects <- simulate_effects.dst(n_tags = n_tags, theta = theta,
                                         distribution = "exponential",
                                         de_prob = de_prob,
@@ -54,31 +55,69 @@ simulate_effects <- function(n_tags, mean_pars = NULL, type = c("dst", "emp"),
 
 #' Simulate effects by sampling from estimated coefficients
 #' @inheritParams simulate_effects
-simulate_effects.emp <- function(n_tags, mean_pars, de_prob, interval = 1) {
-    # sample base means
+simulate_effects.emp <- function(n_tags, mean_pars, interval = 1) {
     b0 <- sample(mean_pars[, 1], n_tags, replace = TRUE)
+
     b1 <- sapply(b0, sample_neighborhood, x = b0, y = mean_pars[, 2],
                  interval = interval)
+    return(cbind(b0, b1))
+}
 
+simulate_effects.dst <- function() {
+    if (sum(de_prob) < 0 || sum(de_prob) > 1)
+        stop("sum(de_prob) must be between 0 and 1")
+    if (length(de_prob) == 1) {
+        message("simulating equal chance of up and down regulation")
+        de_prob <- c(de_prob / 2, de_prob / 2)
+    }
+    de <- sample(c(-1, 0, 1), size = n_tags, replace = TRUE,
+                 prob = c(de_prob['down'], 1 - sum(de_prob), de_prob['up']))
 }
 #' Simulate RNASeq counts
 #' @param mean_pars A matrix of per-tag regression coefficients - should have
 #' two columns, with the first column the intercept
 #' @param dispersion_pars A vector of estimated per-tag dispersions
+#' @param dispersion_interval After simulating the base mean `b0`,
+#' the dispersion is sampled from
+#' `dispersion_pars[abs(mean_pars[, 1] - b0) < dispersion_interval]`
 #' @param n_tags The number of tags to simulate
-#' @param offset_pars A vector of estimated sample offsets
 #' @param design The design matrix for the simulated samples - should have
 #' two columns, with the first column the intercept
+#' @param offset_pars A vector of estimated sample offsets
+#' @param offset_options A list of options to control the behavior of
+#' `simulate_offsets()`
 #' @param effects_options A list of options to control the behavior of
 #' `simulate_effects()`
-simulate_counts <- function(mean_pars, dispersion_pars, n_tags,
-                            offset_pars, design) {
+simulate_counts <- function(mean_pars, dispersion_pars,
+                            dispersion_interval = log(20), n_tags,
+                            design,
+                            offset_pars,
+                            offset_options = list(),
+                            effects_options = list()) {
     n_samples <- nrow(design)
+    offset_args <- c(offset_options, list(n_samples = n_samples))
+    offsets <- do.call(sample_offsets, offset_args)
     effects_args <- c(effects_options, list(n_tags = n_tags))
-    if (!is.null(effects_options$type)) {
-        if (effects_options$type == "emp")
+    if (!is.null(effects_options$method)) {
+        if (effects_options$method == "emp")
             effects_args$mean_pars <- mean_pars
     }
     effects <- do.call(simulate_effects, effects_args)
-    raw_means <- exp(t(design %*% t(effects)))
+    dispersions <- sapply(mean_pars[, 1], sample_neighborhood,
+                          x = mean_pars[, 1], y = dispersion_pars,
+                          interval = dispersion_interval)
+    means <- exp(offsets + t(design %*% t(effects)))
+    counts <- matrix(nrow = n_tags, ncol = n_samples)
+    for (s in seq(1, n_samples)) {
+        counts[, s] <- rnbinom(n_tags, mu = means[, s], size = 1 / dispersions)
+    }
+    return(
+        list(
+            counts = counts,
+            effects = effects,
+            offsets = offsets,
+            dispersions = dispersions,
+            means = means
+        )
+    )
 }
