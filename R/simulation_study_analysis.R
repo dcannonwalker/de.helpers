c67dd8f1f0527a6b955182e39dc63d4b87781062
dcannonwalker <42654459+dcannonwalker@users.noreply.github.com>
2025-08-05 15:49:32 -0500

    add some skeletal functions

---
+++ R/simulation_study_analysis.R
@@ -0,0 +1,22 @@
+#' Construct an ROC curve for a given model fit
+#' @param method_data Model fit for a single method,
+#' as read in by `read_method_data()`
+make_roc_curve <- function(method_data) {
+
+}
+
+#' Construct the average ROC curve for a set of model fits
+#' @inheritParams make_roc_curve
+make_average_roc_curve <- function(method_data, x0) {
+
+}
+
+#' @inheritParams make_roc_curve
+make_fdr_curve <- function(method_data) {
+
+}
+
+#' @inheritParams make_roc_curve
+make_average_fdr_curve <- function(method_data, x0) {
+
+}
